bea9067a40ce57141a7b56eecf1ec251143dbd9b
Andrew <fleming-andrew@protonmail.com>
2021-07-18 21:43:32 -0400

    Added init diamond test/script/config

---
+++ test/Diamond.test.js
@@ -0,0 +1,72 @@
+/* global describe it before ethers */
+
+const {
+    getSelectors,
+    FacetCutAction,
+    removeSelectors,
+    findAddressPositionInFacets
+  } = require('../scripts/libraries/diamond.js')
+  
+  const { deployDiamond } = require('../scripts/deployDiamond.ts')
+  
+  const { assert } = require('chai')
+  
+  describe('DiamondTest', async function () {
+    let diamondAddress
+    let diamondCutFacet
+    let diamondLoupeFacet
+    let ownershipFacet
+    let tx
+    let receipt
+    let result
+    const addresses = []
+  
+    before(async function () {
+      diamondAddress = await deployDiamond()
+      diamondCutFacet = await ethers.getContractAt('DiamondCutFacet', diamondAddress)
+      diamondLoupeFacet = await ethers.getContractAt('DiamondLoupeFacet', diamondAddress)
+      ownershipFacet = await ethers.getContractAt('OwnershipFacet', diamondAddress)
+    })
+  
+    it('should have three facets -- call to facetAddresses function', async () => {
+      for (const address of await diamondLoupeFacet.facetAddresses()) {
+        addresses.push(address)
+      }
+  
+      assert.equal(addresses.length, 3)
+    })
+  
+    it('facets should have the right function selectors -- call to facetFunctionSelectors function', async () => {
+      let selectors = getSelectors(diamondCutFacet)
+      result = await diamondLoupeFacet.facetFunctionSelectors(addresses[0])
+      assert.sameMembers(result, selectors)
+      selectors = getSelectors(diamondLoupeFacet)
+      result = await diamondLoupeFacet.facetFunctionSelectors(addresses[1])
+      assert.sameMembers(result, selectors)
+      selectors = getSelectors(ownershipFacet)
+      result = await diamondLoupeFacet.facetFunctionSelectors(addresses[2])
+      assert.sameMembers(result, selectors)
+    })
+  
+    it('selectors should be associated to facets correctly -- multiple calls to facetAddress function', async () => {
+      assert.equal(
+        addresses[0],
+        await diamondLoupeFacet.facetAddress('0x1f931c1c')
+      )
+      assert.equal(
+        addresses[1],
+        await diamondLoupeFacet.facetAddress('0xcdffacc6')
+      )
+      assert.equal(
+        addresses[1],
+        await diamondLoupeFacet.facetAddress('0x01ffc9a7')
+      )
+      assert.equal(
+        addresses[2],
+        await diamondLoupeFacet.facetAddress('0xf2fde38b')
+      )
+    })
+  
+    
+  })
+  
